7bb64dfa5ca992d828d9d1d435510d2e2a10d4e2
Yavuz Cingöz <68475175+yavuz2525@users.noreply.github.com>
2026-08-15 21:19:22 +0300

    Expose settings and updater IPC

--- preload.js
+++ preload.js
@@ -8,9 +8,18 @@ contextBridge.exposeInMainWorld('clipdeck', {
   clear: () => ipcRenderer.invoke('history:clear'),
   setPaused: (paused) => ipcRenderer.invoke('settings:paused', paused),
   setLimit: (limit) => ipcRenderer.invoke('settings:limit', limit),
+  setShortcut: (shortcut) => ipcRenderer.invoke('settings:shortcut', shortcut),
+  getUpdateState: () => ipcRenderer.invoke('updates:get'),
+  checkForUpdates: () => ipcRenderer.invoke('updates:check'),
+  installUpdate: () => ipcRenderer.invoke('updates:install'),
   onChanged: (callback) => {
     const listener = (_event, state) => callback(state);
     ipcRenderer.on('history:changed', listener);
     return () => ipcRenderer.removeListener('history:changed', listener);
+  },
+  onUpdateChanged: (callback) => {
+    const listener = (_event, state) => callback(state);
+    ipcRenderer.on('updates:changed', listener);
+    return () => ipcRenderer.removeListener('updates:changed', listener);
   },
 });
